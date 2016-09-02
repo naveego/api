f76fd05d4267d1ec61538010eed5ce5c3c6ad40f
Derek Smith <derek.smith@naveego.com>
2016-09-02 15:49:46 -0400

    Updated principal.go

--- types/principal.go
+++ types/principal.go
@@ -9,3 +9,13 @@ type Principal struct {
 	Repository string   `json:"repository"` // The repository the principal belongs bool
 	Roles      []string `json:"roles"`      // The roles the prinicpal fulfills
 }
+
+// IsInRole checks to see if the principal fulfills a given role
+func (p Principal) IsInRole(role string) bool {
+	for _, r := range p.Roles {
+		if r == role {
+			return true
+		}
+	}
+	return false
+}
