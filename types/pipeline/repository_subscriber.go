97b8ce8f0fcfec39bdc8ad82f333212d62c28935
Derek Smith <derek.smith@naveego.com>
2016-09-02 17:44:25 -0400

    Updated subscriber

--- types/pipeline/repository_subscriber.go
+++ types/pipeline/repository_subscriber.go
@@ -4,6 +4,8 @@ package pipeline
 type RepositorySubscriber struct {
 	ID          string                 `json:"id" bson:"_id,omitempty"` // The ID of the Subscriber
 	Name        string                 `json:"name"`                    // The Name of the Subscriber
+	SafeName    string                 `json:"safe_name"`               // The Safe name of the Subscriber
+	Repository  string                 `json:"repository"`              // The Repository the subscribe belongs to
 	Description string                 `json:"description"`             // The Description of the Subscriber
 	Type        string                 `json:"type"`                    // The Type of the Subscriber
 	InputStream string                 `json:"input"`                   // The input stream to consume
