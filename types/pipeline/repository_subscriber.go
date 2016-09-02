fae7547afba998af4ff6989e758c313941cc57ae
Derek Smith <derek.smith@naveego.com>
2016-09-02 17:19:55 -0400

    Updated repository subscriber

--- types/pipeline/repository_subscriber.go
+++ types/pipeline/repository_subscriber.go
@@ -6,6 +6,7 @@ type RepositorySubscriber struct {
 	Name        string                 `json:"name"`                    // The Name of the Subscriber
 	Description string                 `json:"description"`             // The Description of the Subscriber
 	Type        string                 `json:"type"`                    // The Type of the Subscriber
+	InputStream string                 `json:"input"`                   // The input stream to consume
 	Shapes      map[string]Shape       `json:"shapes"`                  // The Shapes accepted by this Subscriber
 	Settings    map[string]interface{} `json:"settings"`                // The settings of the Subscriber
 }
