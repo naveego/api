1cd294927b04f916414eff78811bafbbfef8a276
Derek Smith <derek.smith@naveego.com>
2016-09-02 16:36:24 -0400

    Updated bson mappings

--- types/pipeline/repository_publisher.go
+++ types/pipeline/repository_publisher.go
@@ -2,13 +2,13 @@ package pipeline
 
 // RepositoryPublisher represents an publisher that is configured for a given repository
 type RepositoryPublisher struct {
-	ID          string                 `json:"id"`          // The ID of the Publisher
-	Name        string                 `json:"name"`        // The Name of the Publisher
-	SafeName    string                 `json:"safe_name"`   // The Safe name of the publisehr
-	Repository  string                 `json:"repository"`  // The Repository the publisher belongs too
-	Description string                 `json:"description"` // The Description of the Publisher
-	Type        string                 `json:"type"`        // The Type of the Publisher
-	IconURL     string                 `json:"icon"`        // The Icon of the Publisher
-	Shapes      map[string]Shape       `json:"shapes"`      // The Shapes publsihed by this Publisher
-	Settings    map[string]interface{} `json:"settings"`    // The settings of the Publisher
+	ID          string                 `json:"id" bson:"_id,omitempty"` // The ID of the Publisher
+	Name        string                 `json:"name"`                    // The Name of the Publisher
+	SafeName    string                 `json:"safe_name"`               // The Safe name of the publisehr
+	Repository  string                 `json:"repository"`              // The Repository the publisher belongs too
+	Description string                 `json:"description,omitempty"`   // The Description of the Publisher
+	Type        string                 `json:"type"`                    // The Type of the Publisher
+	IconURL     string                 `json:"icon"`                    // The Icon of the Publisher
+	Shapes      map[string]Shape       `json:"shapes"`                  // The Shapes publsihed by this Publisher
+	Settings    map[string]interface{} `json:"settings"`                // The settings of the Publisher
 }
--- types/pipeline/repository_subscriber.go
+++ types/pipeline/repository_subscriber.go
@@ -2,10 +2,10 @@ package pipeline
 
 // RepositorySubscriber represents an invocable subscriber that is defined inside a specific repository
 type RepositorySubscriber struct {
-	ID          string                 `json:"id"`          // The ID of the Subscriber
-	Name        string                 `json:"name"`        // The Name of the Subscriber
-	Description string                 `json:"description"` // The Description of the Subscriber
-	Type        string                 `json:"type"`        // The Type of the Subscriber
-	Shapes      map[string]Shape       `json:"shapes"`      // The Shapes accepted by this Subscriber
-	Settings    map[string]interface{} `json:"settings"`    // The settings of the Subscriber
+	ID          string                 `json:"id" bson:"_id,omitempty"` // The ID of the Subscriber
+	Name        string                 `json:"name"`                    // The Name of the Subscriber
+	Description string                 `json:"description"`             // The Description of the Subscriber
+	Type        string                 `json:"type"`                    // The Type of the Subscriber
+	Shapes      map[string]Shape       `json:"shapes"`                  // The Shapes accepted by this Subscriber
+	Settings    map[string]interface{} `json:"settings"`                // The settings of the Subscriber
 }
