4cda3374c02706509e35a0c6333e4a392dc08ffd
Derek Smith <derek.smith@naveego.com>
2016-09-02 15:14:01 -0400

    Updated names

--- types/pipeline/pipeline.go
+++ types/pipeline/pipeline.go
@@ -19,23 +19,23 @@ type PipelineStatus string
 // in Naveego pipeline.  It controls the flow of data from one activity
 // to another.
 type Pipeline struct {
-	ID          string             `json:"id"`                    // The ID of the pipeline
-	Name        string             `json:"name"`                  // The Name of the pipeline
-	Description string             `json:"description,omitempty"` // The description of the piepline
-	Status      PipelineStatus     `json:"status"`                // The Status of the pipeline
-	Publisher   PipelinePublisher  `json:"publisher"`             // The Publisher for the pipeline
-	Subscriber  PipelineSubscriber `json:"subscriber"`            // The Subscriber for the pipeline
-	Activities  []Activity         `json:"activities"`            // The activities in the pipeline
-	CreatedOn   string             `json:"created_on,omitempty"`  // The date the pipeline was created on
-	ModifiedOn  string             `json:"modified_on,omitempty"` // The date the pipeline was modified
+	ID          string           `json:"id"`                    // The ID of the pipeline
+	Name        string           `json:"name"`                  // The Name of the pipeline
+	Description string           `json:"description,omitempty"` // The description of the piepline
+	Status      PipelineStatus   `json:"status"`                // The Status of the pipeline
+	Publisher   PublisherStream  `json:"publisher"`             // The Publisher for the pipeline
+	Subscriber  SubscriberStream `json:"subscriber"`            // The Subscriber for the pipeline
+	Activities  []Activity       `json:"activities"`            // The activities in the pipeline
+	CreatedOn   string           `json:"created_on,omitempty"`  // The date the pipeline was created on
+	ModifiedOn  string           `json:"modified_on,omitempty"` // The date the pipeline was modified
 }
 
-type PipelinePublisher struct {
+type PublisherStream struct {
 	ID           string `json:"publisher_id" bson:"publisher_id"` // The ID of the Publisher
 	OutputStream string `json:"output" bson:"output"`             // The stream that the publisher will publish to
 }
 
-type PipelineSubscriber struct {
+type SubscriberStream struct {
 	ID          string `json:"subscriber_id" bson:"subscriber_id"` // The ID of the Subscriber
 	InputStream string `json:"input" bson:"input"`                 // The input stream to send data to
 }
