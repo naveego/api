107d06eaba330f27bf60eeb2b81ad7290de30024
Derek Smith <derek.smith@naveego.com>
2016-09-15 16:46:56 -0400

    Updated queue.Message

--- types/queue/message.go
+++ types/queue/message.go
@@ -1,6 +1,13 @@
 package queue
 
+import "time"
+
 type Message struct {
-	ID   string                 `json:"id"`   // The Messages ID
-	Data map[string]interface{} `json:"data"` // The data for the Message
+	ID         string                 `json:"id" bson:"_id"`      // The Messages ID
+	Queue      string                 `json:"queue" bson:"queue"` // The Queue ID for the message
+	Data       map[string]interface{} `json:"data" bson:"data"`   // The data for the Message
+	Date       time.Time              `json:"-" bson:"date"`      // The date the message was queued
+	InProgress bool                   `json:"-" bson:"_inProg"`   // Indicator if this messages is in progress
+	ExpireDate time.Time              `json:"-" bson:"_exp"`      // When this message needs to be done by
+	Done       bool                   `json:"-" bson:"_done"`     // Whether or not this message is complete
 }
