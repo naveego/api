2a0f31bfec25fb18234fd0e2e3bc2c3423df0535
Derek Smith <derek.smith@naveego.com>
2016-09-30 13:23:40 -0400

    Updated bucket

--- types/pipeline/bucket.go
+++ types/pipeline/bucket.go
@@ -4,15 +4,10 @@ package pipeline
 // into the pipeline.  A repository can only have one Bucket
 // defined.  A bucket is also optional.
 type Bucket struct {
-	ID          string           `json:"-" bson:"_id,omitempty"`
-	Status      PipelineStatus   `json:"status" bson:"status"`         // The Status of the pipeline
-	InputStream string           `json:"input" bson:"input"`           // The input stream
-	Subscriber  BucketSubscriber `json:"subscriber" bson:"subscriber"` // The subcriber bucket
-}
-
-type BucketSubscriber struct {
-	Type           string                 `json:"type" bson:"type"`
-	StreamEndpoint string                 `json:"stream_endpoint" bson:"stream_endpoint"`
-	Shapes         map[string]Shape       `json:"shapes" bson:"shapes"`
-	Settings       map[string]interface{} `json:"settings" bson:"settings"`
+	ID          string                 `json:"-" bson:"_id,omitempty"`
+	Status      PipelineStatus         `json:"status" bson:"status"` // The Status of the pipeline
+	InputStream string                 `json:"input" bson:"input"`   // The input stream
+	Type        string                 `json:"type" bson:"type"`     // The type of subscriber to use
+	Shapes      map[string]Shape       `json:"shapes" bson:"shapes"`
+	Settings    map[string]interface{} `json:"settings" bson:"settings"`
 }
