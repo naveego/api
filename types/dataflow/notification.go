b342a4a0617a9d27e5b308ce1432c83ba4749430
Derek Smith <derek.smith@naveego.com>
2017-09-18 19:31:09 -0400

    Added label

--- types/dataflow/notification.go
+++ types/dataflow/notification.go
@@ -9,7 +9,9 @@ import (
 // Notification represents a scheduled notification in the data flow log
 type Notification struct {
 	ID        string    `json:"id" bson:"_id"`
-	TenantID  string    `json:"-" bson:"tenant_id"`
+	TenantID  string    `json:"tenant_id" bson:"tenant_id"`
+	UserID    string    `json:"user_id,omitempty" bson:"user_id"`
+	Label     string    `json:"label" bson:"label"`
 	Type      string    `json:"type" bson:"type"`
 	Filter    string    `json:"filter" bson:"filter"`
 	Topic     string    `json:"topic" bson:"topic"`
@@ -19,12 +21,21 @@ type Notification struct {
 // Validate checks to make sure we have valid information on
 // the notification.
 func (n *Notification) Validate() error {
+
+	if n.TenantID == "" {
+		return errors.NewWithCode(4000001, "missing tenant_id")
+	}
+
+	if n.Label == "" {
+		return errors.NewWithCode(4000002, "missing label")
+	}
+
 	if n.Filter == "" {
-		return errors.NewWithCode(4000001, "Invalid notification: missing filter")
+		return errors.NewWithCode(4000003, "missing filter")
 	}
 
 	if n.Topic == "" {
-		return errors.NewWithCode(4000002, "Invalid notification: missing topic")
+		return errors.NewWithCode(4000004, "missing topic")
 	}
 
 	return nil
--- types/dataflow/notification_test.go
+++ types/dataflow/notification_test.go
@@ -9,10 +9,29 @@ import (
 
 func Test_Notification_Validate(t *testing.T) {
 
-	Convey("Given a notification with a blank topic", t, func() {
+	Convey("Given a notification with a blank tenant_id", t, func() {
 		note := &Notification{
-			Filter: "level:info",
-			Topic:  "",
+			TenantID: "",
+			Label:    "My Notification",
+			Filter:   "level:info",
+			Topic:    "nrn:test",
+		}
+		err := note.Validate()
+
+		Convey("should return a validation error", func() {
+			So(err, ShouldNotBeNil)
+
+			e, _ := err.(errors.Error)
+			So(e.Code, ShouldEqual, 4000001)
+		})
+	})
+
+	Convey("Given a notification with a blank label", t, func() {
+		note := &Notification{
+			TenantID: "vandelay",
+			Label:    "",
+			Filter:   "level:info",
+			Topic:    "nrn:test",
 		}
 		err := note.Validate()
 
@@ -26,8 +45,10 @@ func Test_Notification_Validate(t *testing.T) {
 
 	Convey("Given a notification with a blank filter", t, func() {
 		note := &Notification{
-			Filter: "",
-			Topic:  "nrn:test",
+			TenantID: "vandelay",
+			Label:    "My Notification",
+			Filter:   "",
+			Topic:    "nrn:test",
 		}
 		err := note.Validate()
 
@@ -35,7 +56,24 @@ func Test_Notification_Validate(t *testing.T) {
 			So(err, ShouldNotBeNil)
 
 			e, _ := err.(errors.Error)
-			So(e.Code, ShouldEqual, 4000001)
+			So(e.Code, ShouldEqual, 4000003)
+		})
+	})
+
+	Convey("Given a notification with a blank topic", t, func() {
+		note := &Notification{
+			TenantID: "vandelay",
+			Label:    "My Notification",
+			Filter:   "level:info",
+			Topic:    "",
+		}
+		err := note.Validate()
+
+		Convey("should return a validation error", func() {
+			So(err, ShouldNotBeNil)
+
+			e, _ := err.(errors.Error)
+			So(e.Code, ShouldEqual, 4000004)
 		})
 	})
 
