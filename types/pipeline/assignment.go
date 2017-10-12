161b4c8b8b0f62870f1d49968b55f2f552dc6832
AzureAD\SteveRuble <steve.ruble@naveego.com>
2017-10-12 16:46:49 -0400

    Added BSON tags.

--- types/pipeline/assignment.go
+++ types/pipeline/assignment.go
@@ -27,14 +27,14 @@ const (
 // Assignment represents the assignment of a publisher or subscriber
 // (or other segment) to an agent where it will run.
 type Assignment struct {
-	ID              string           `json:"id,omitempty" bson:"_id,omitempty"` // The ID of the assignment.
-	AgentID         string           `json:"agent_id,omitempty"`                // The ID of the agent.
-	SegmentID       string           `json:"segment_id,omitempty"`              // The ID of the segment.
-	Type            string           `json:"type,omitempty"`                    // The segment type, probably "publisher" or "subscriber".
-	Status          AssignmentStatus `json:"status,omitempty"`
-	CreatedAt       time.Time        `json:"created_at,omitempty"`        // The UTC time this assignment was created.
-	StartedAt       time.Time        `json:"started_at,omitempty"`        // The UTC time the segment started running on the agent.
-	LastHeartbeatAt time.Time        `json:"last_heartbeat_at,omitempty"` // The most recent UTC time the agent signalled that the segment was healthy.
+	ID              string           `json:"id,omitempty" bson:"_id,omitempty"`    // The ID of the assignment.
+	AgentID         string           `json:"agentID,omitempty" bson:"agentID"`     // The ID of the agent.
+	SegmentID       string           `json:"segmentID,omitempty" bson:"segmentID"` // The ID of the segment.
+	Type            string           `json:"type,omitempty" bson:"type"`           // The segment type, probably "publisher" or "subscriber".
+	Status          AssignmentStatus `json:"status,omitempty" bson:"status"`
+	CreatedAt       time.Time        `json:"created_at,omitempty" bson:"createdAt"`                        // The UTC time this assignment was created.
+	StartedAt       time.Time        `json:"started_at,omitempty" bson:"startedAt,omitempty"`              // The UTC time the segment started running on the agent.
+	LastHeartbeatAt time.Time        `json:"last_heartbeat_at,omitempty" bson:"lastHeartbeatAt,omitempty"` // The most recent UTC time the agent signalled that the segment was healthy.
 }
 
 // Validate returns an error if the Assigment is invalid.
