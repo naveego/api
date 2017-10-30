a240fd74c645d5e480a2c7f8bef1972f66a30883
AzureAD\SteveRuble <SteveRuble@users.noreply.github.com>
2017-10-30 16:37:42 -0400

    Added details to Agent struct.

--- types/pipeline/agent.go
+++ types/pipeline/agent.go
@@ -6,8 +6,11 @@ import (
 
 // Agent contains information about an agent which can run pipeline segments.
 type Agent struct {
-	ID         string    `json:"id,omitempty" bson:"_id,omitempty"` // The ID of the Agent.
-	Host       string    `json:"host,omitempty"`                    // The Host the Agent is running on.
-	OnPremises bool      `json:"onPremise,omitempty"`               // True if the Agent is running within the tenant's firewall.
-	CreatedAt  time.Time `json:"createdAt,omitempty"`
+	ID           string    `json:"id,omitempty" bson:"_id,omitempty"` // The ID of the Agent.
+	Host         string    `json:"host,omitempty"`                    // The Host the Agent is running on.
+	OS           string    `json:"os,omitempty"`
+	Arch         string    `json:"arch,omitempty"`
+	Capabilities []string  `json:"capabilities,omitempty"`
+	OnPremises   bool      `json:"onPremise,omitempty"` // True if the Agent is running within the tenant's firewall.
+	CreatedAt    time.Time `json:"createdAt,omitempty"`
 }
