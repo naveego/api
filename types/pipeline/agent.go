2bbb1ebf600f92293d8a719aeac5673ea00129f8
Derek Smith <derek.smith@naveego.com>
2017-11-17 12:29:48 -0500

    PIPEUI-3 #comment Added Name property to agent

--- types/pipeline/agent.go
+++ types/pipeline/agent.go
@@ -8,9 +8,10 @@ import (
 type Agent struct {
 	ID           string    `json:"id,omitempty" bson:"_id,omitempty"` // The ID of the Agent.
 	Host         string    `json:"host,omitempty"`                    // The Host the Agent is running on.
+	Name         string    `json:"name,omitempty"`                    // The Name of the host
 	OS           string    `json:"os,omitempty"`
 	Arch         string    `json:"arch,omitempty"`
 	Capabilities []string  `json:"capabilities,omitempty"`
-	OnPremises   bool      `json:"onPremise,omitempty"` // True if the Agent is running within the tenant's firewall.
+	OnPremises   bool      `json:"onPremises,omitempty"` // True if the Agent is running within the tenant's firewall.
 	CreatedAt    time.Time `json:"createdAt,omitempty"`
 }
