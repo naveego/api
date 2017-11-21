acccfa5222472a4d6ae2e270ff7ab9d3d4197987
Derek Smith <derek.smith@naveego.com>
2017-11-21 13:46:44 -0500

    Added Tags, removed Capabilities and OnPremises

--- types/pipeline/agent.go
+++ types/pipeline/agent.go
@@ -6,12 +6,11 @@ import (
 
 // Agent contains information about an agent which can run pipeline segments.
 type Agent struct {
-	ID           string    `json:"id,omitempty" bson:"_id,omitempty"` // The ID of the Agent.
-	Host         string    `json:"host,omitempty"`                    // The Host the Agent is running on.
-	Name         string    `json:"name,omitempty"`                    // The Name of the host
-	OS           string    `json:"os,omitempty"`
-	Arch         string    `json:"arch,omitempty"`
-	Capabilities []string  `json:"capabilities,omitempty"`
-	OnPremises   bool      `json:"onPremises,omitempty"` // True if the Agent is running within the tenant's firewall.
-	CreatedAt    time.Time `json:"createdAt,omitempty"`
+	ID        string    `json:"id,omitempty" bson:"_id,omitempty"` // The ID of the Agent.
+	Host      string    `json:"host,omitempty"`                    // The Host the Agent is running on.
+	Name      string    `json:"name,omitempty"`                    // The Name of the host
+	OS        string    `json:"os,omitempty"`
+	Arch      string    `json:"arch,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+	Tags      []string  `json:"tags,omitempty"`
 }
