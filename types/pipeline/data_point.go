a2a1e79d7399b6994f77c51e2880a136322cf356
Steve Ruble <SteveRuble@users.noreply.github.com>
2018-02-26 08:45:08 -0500

    PIPEUI-8 added additional actions

--- types/pipeline/data_point.go
+++ types/pipeline/data_point.go
@@ -15,6 +15,27 @@ const (
 	DataPointUpsert DataPointAction = "upsert"
 	// DataPointDelete represents a delete action
 	DataPointDelete DataPointAction = "delete"
+
+	// DataPointStartPublish represents the start of a publish operation.
+	// The Data field will be a map of PropertyDefinition.ID to PropertyDefinition.Name
+	// The subscriber should drop and recreate the destination table when this
+	// action is received
+	DataPointStartPublish DataPointAction = "start-publish"
+
+	// DataPointEndPublish represents the normal end of a publish operation.
+	DataPointEndPublish DataPointAction = "end-publish"
+
+	// DataPointAbendPublish represents an unexpected end of a publish operation.
+	DataPointAbendPublish DataPointAction = "abend"
+
+	// DataPointMalformed represents a data point which should be logged but which
+	// the subscriber should not attempt to insert into the target database.
+	DataPointMalformed DataPointAction = "malformed"
+
+	// DataPointSample represents a data point which was created as part
+	// of a discovery or editing process and should not be inserted into
+	// the destination table.
+	DataPointSample DataPointAction = "sample"
 )
 
 func (d *DataPointAction) UnmarshalJSON(bytes []byte) error {
@@ -84,12 +105,13 @@ func (d *DataPoint) Validate() error {
 		return errors.Error{Code: NoActionError, Message: "no action was defined"}
 	}
 
-	if d.KeyNames == nil || len(d.KeyNames) == 0 {
-		return errors.Error{Code: NoKeyNamesError, Message: "keyNames was either not provided or is empty"}
+	if d.Action != DataPointUpsert && d.Action != DataPointDelete {
+		// Only validate data and shape on upsert and delete
+		return nil
 	}
 
-	if d.Action != DataPointUpsert && d.Action != DataPointDelete {
-		return errors.Error{Code: InvalidActionError, Message: "invalid action"}
+	if d.KeyNames == nil || len(d.KeyNames) == 0 {
+		return errors.Error{Code: NoKeyNamesError, Message: "keyNames was either not provided or is empty"}
 	}
 
 	if d.Data == nil || len(d.Data) == 0 {
